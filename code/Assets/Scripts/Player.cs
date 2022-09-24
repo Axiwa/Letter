cb4524aaebf36d79a55775079bad804115c2b3c0
Axiwa <280253124@qq.com>
2022-09-24 14:52:30 +0200

    jump

--- code/Assets/Scripts/Player.cs
+++ code/Assets/Scripts/Player.cs
@@ -2,52 +2,75 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player{
-    private int health = 100;
+public class Player : MonoBehaviour
+{
+    [SerializeField]
+    private float moveForce = 10f;
+    [SerializeField]
+    private float jumpForce = 11f;
+    private Rigidbody2D myBody;
+    private Animator anim;
+    private string WALK_ANIMATION = "Walk";
+    private bool isGrounded = true;
+    private string GROUND_TAG = "Ground";
+    private SpriteRenderer sr;
+    private float movementX = 22f;
 
-    public int Health{
-        get{
-            return health;
+
+    private void Awake(){
+        myBody = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        sr = GetComponent<SpriteRenderer>();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void FixedUpdate() {
+        PlayerJump();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        PlayerMOveKeyboard();
+        AnimatePlayer();
+        PlayerJump();
+    }
+
+    void PlayerMOveKeyboard(){
+        movementX = Input.GetAxisRaw("Horizontal"); // GetAxis: not only -1 and 1
+        
+        transform.position += new Vector3(movementX, 0f, 0f) * Time.deltaTime * moveForce;
+    }
+
+    void AnimatePlayer(){
+        if (movementX > 0){
+            anim.SetBool(WALK_ANIMATION, true);
+            sr.flipX = false;
         }
-        set{
-            health = value;
+        else if (movementX < 0){
+            anim.SetBool(WALK_ANIMATION, true);
+            sr.flipX = true;
+        }
+        else{
+            anim.SetBool(WALK_ANIMATION, false);
+        }
+        
+    }
+
+    void PlayerJump(){
+        if (Input.GetButtonDown("Jump") && isGrounded){ // GetButtonUp: once you leave the button // GetButton: you hold and it will continue to be triggered
+            isGrounded = false;
+            myBody.AddForce(new Vector2(0f, jumpForce), ForceMode2D.Impulse);
         }
     }
 
-    int power = 50;
-
-    public int Power{
-        get{
-            return power;
+    private void OnCollisionEnter2D(Collision2D other) {
+        if (other.gameObject.CompareTag(GROUND_TAG)){
+            isGrounded = true;
         }
-        set{
-            power = value;
-        }
-    }    
-    string name = "Warrior";
-
-    public string Name{
-        get{
-            return name;
-        }
-        set{
-            name = value;
-        }
-    }
-    public Player(){ }
-    public Player(int health, int power, string name){
-        Health = health;
-        Power = power;
-        Name = name;
-    }
-
-    public virtual void Attack(){
-        Debug.Log(Name + " is attacking you!\n");
-    }
-
-    public void Info(){
-        Debug.Log("Health is: " + Health);
-        Debug.Log("Power is: " + power);
-        Debug.Log("Name is: " + name);        
     }
 }
